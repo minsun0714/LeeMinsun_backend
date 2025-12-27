119ba41fdc790e1d897c544adb22e45dcc500511
이민선 <woooooooow22@gmail.com>
2025-12-27 15:43:17 +0900

    feat: 계좌 생성 도메인 모델 및 생성 DTO 추가

--- src/main/java/com/wirebarley/codingtest/account/domain/Account.java
+++ src/main/java/com/wirebarley/codingtest/account/domain/Account.java
@@ -1,16 +1,22 @@
 package com.wirebarley.codingtest.account.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+import static org.springframework.util.Assert.state;
+
 @Entity
 @Table(name = "accounts")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Account {
 
     @Id
@@ -34,4 +40,25 @@ public class Account {
     @UpdateTimestamp
     @Column(nullable = true)
     private OffsetDateTime updatedAt;
+
+    public static Account create(
+            String accountNumber,
+            BigDecimal initialBalance
+    ) {
+        validate(accountNumber, initialBalance);
+
+        Account account = new Account();
+
+        account.accountNumber = accountNumber;
+        account.balance = initialBalance;
+        account.status = AccountStatus.ACTIVE;
+
+        return account;
+    }
+
+    private static void validate(String accountNumber, BigDecimal initialBalance){
+        state(accountNumber != null && !accountNumber.isBlank(), "계좌번호는 필수입니다.");
+        state(initialBalance != null, "초기 잔액은 필수입니다.");
+        state(initialBalance.compareTo(BigDecimal.ZERO) >= 0, "초기 잔액은 음수가 될 수 없습니다.");
+    }
 }
