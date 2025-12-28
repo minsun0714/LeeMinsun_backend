9fbbafabdeff13af9fd936f48e8be0fa5a10e344
이민선 <woooooooow22@gmail.com>
2025-12-28 10:54:44 +0900

    refactor: EntityNotFoundException을 도메인 exception으로 수정

--- src/main/java/com/wirebarley/codingtest/account/domain/exception/AccountExceptionMessage.java
+++ src/main/java/com/wirebarley/codingtest/account/domain/exception/AccountExceptionMessage.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum AccountExceptionMessage {
+    ACCOUNT_NOT_EXIST("존재하지 않는 계좌입니다.", HttpStatus.BAD_REQUEST),
     ACCOUNT_NUMBER_REQUIRED("계좌번호는 필수입니다.", HttpStatus.BAD_REQUEST),
     INVALID_INITIAL_BALANCE("초기 잔액은 0 이상이어야 합니다.", HttpStatus.BAD_REQUEST),
     ACCOUNT_ALREADY_CLOSED("이미 삭제된 계좌입니다.", HttpStatus.BAD_REQUEST),
