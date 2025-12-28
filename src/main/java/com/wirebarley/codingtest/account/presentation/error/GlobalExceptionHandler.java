40bfa602ff0fb70a18c295b6d8f4e2ddfc6e4211
이민선 <woooooooow22@gmail.com>
2025-12-28 13:19:37 +0900

    feat: 잘못된 요청에 대한 전역 예외 처리 추가
    - HttpMessageNotReadableException 처리 추가
    - MethodArgumentNotValidException 처리 추가
    - INVALID_REQUEST 공통 ErrorResponse 반환

--- src/main/java/com/wirebarley/codingtest/account/presentation/error/GlobalExceptionHandler.java
+++ src/main/java/com/wirebarley/codingtest/account/presentation/error/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import com.wirebarley.codingtest.common.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,6 +21,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getStatus())
                 .body(ErrorResponse.from(e));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidJson(HttpMessageNotReadableException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(ErrorResponse.notReadable());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(ErrorResponse.validate());
     }
 
     @ExceptionHandler(Exception.class)
